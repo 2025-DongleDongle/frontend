465519afcf46cd31157ac71ce991dada2ef9ed06
Min-hye-Kim <kimilu@ewha.ac.kr>
2025-11-22 23:28:57 +0900

    ✨ feat : AcctSummaryProfileEdit 페이지 퍼블리싱 및 App.jsx 경로 추가 (#82)

--- src/App.jsx
+++ src/App.jsx
@@ -29,6 +29,7 @@ import AcctSummaryLoading from "./pages/AcctSummaryLoading";
 import AcctSummaryComplete from "./pages/AcctSummaryComplete";
 import AcctSummaryProfileData from "./pages/AcctSummaryProfileData";
 import AcctSummaryPage from "./pages/AcctSummaryPage";
+import AcctSummaryProfileEdit from "./pages/AcctSummaryProfileEdit";
 
 function App() {
   return (
@@ -37,7 +38,8 @@ function App() {
         <Routes>
           {/* NavTopbar(네비게이션) */}
           <Route element={<MainLayout />}>
-            <Route path="/" element={<Navigate to="/home" replace />} /> {/* 기본 경로를 /home으로 리다이렉트 */}
+            <Route path="/" element={<Navigate to="/home" replace />} />{" "}
+            {/* 기본 경로를 /home으로 리다이렉트 */}
             <Route path="/home" element={<HomePage />} />
             <Route path="/scrapbook" element={<ScrapbookPage />} />
             <Route path="/budget" element={<BudgetPage />} />
@@ -50,19 +52,23 @@ function App() {
 
           {/* BackTopbar(뒤로가기) */}
           <Route element={<BackLayout />}>
-            <Route path="/profile" element={<ProfilePage/>} />
-            <Route path="/summaries/profile" element={<AcctSummaryProfileData />} />
+            <Route path="/profile" element={<ProfilePage />} />
+            <Route
+              path="/summaries/profile"
+              element={<AcctSummaryProfileData />}
+            />
           </Route>
 
           {/* Topbar X */}
           <Route path="/login" element={<LoginPage />} />
           <Route path="/signup" element={<SignupPage />} />
-          <Route path="/signup/complete" element={<SignupCompletePage/>}/>
+          <Route path="/signup/complete" element={<SignupCompletePage />} />
 
           {/* 가계부 요약본 */}
           <Route path="/summaries/loading" element={<AcctSummaryLoading />} />
           <Route path="/summaries/complete" element={<AcctSummaryComplete />} />
           <Route path="/summaries/snapshot" element={<AcctSummaryPage />} />
+          <Route path="/summaries/edit" element={<AcctSummaryProfileEdit />} />
         </Routes>
       </Router>
     </>
